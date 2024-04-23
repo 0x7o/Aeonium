07e90510009579346bc90a807921b0ecaff88ac4
0x7o <danil.kononyuk@ya.ru>
2024-04-23 18:00:10 +0800

    Changes

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -3,7 +3,13 @@ from tokenizers import ByteLevelBPETokenizer
 from transformers import PreTrainedTokenizerFast
 
 dataset1 = load_dataset("IlyaGusev/rulm")["train"]
+dataset1 = dataset1.remove_columns([
+        col for col in dataset1.column_names if col != "text"
+    ])
 dataset2 = load_dataset("code_search_net", "all")["test"]
+dataset2 = dataset2.remove_columns([
+        col for col in dataset2.column_names if col != "whole_func_string"
+    ])
 
 tokenizer = ByteLevelBPETokenizer()
 
@@ -12,13 +18,15 @@ batch_size = 1000
 
 def batch_iterator():
     for i in range(0, len(dataset1), batch_size):
-        yield dataset1[i: i + batch_size]["text"]
+        yield dataset1[i : i + batch_size]["text"]
 
+    for i in range(0, len(dataset2), batch_size):
+        yield dataset2[i : i + batch_size]["whole_func_string"]
 
 
 new_tokenizer = tokenizer.train_from_iterator(
     batch_iterator(),
-    vocab_size=64000,
+    vocab_size=128000,
     special_tokens=[
         "<|end_of_text|>",
         "<|im_end|>",
@@ -26,6 +34,9 @@ new_tokenizer = tokenizer.train_from_iterator(
     ],
 )
 
-tokenizer = PreTrainedTokenizerFast(tokenizer_object=new_tokenizer, bos_token="<|end_of_text|>",
-                                    eos_token="<|end_of_text|>", )
+tokenizer = PreTrainedTokenizerFast(
+    tokenizer_object=new_tokenizer,
+    bos_token="<|end_of_text|>",
+    eos_token="<|end_of_text|>",
+)
 tokenizer.push_to_hub("aeonium/Aeonium-v1-Base-7B")
