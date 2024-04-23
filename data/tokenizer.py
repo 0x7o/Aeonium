e270e5e1f51c1e91decb89093f7ffd8594b48483
0x7o <danil.kononyuk@ya.ru>
2024-04-23 17:55:23 +0800

    Changes

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer
+from tokenizers import SentencePieceBPETokenizer
+from transformers import PreTrainedTokenizerFast
 
 dataset1 = load_dataset("IlyaGusev/rulm")["train"]
 dataset2 = load_dataset("code_search_net", "all")["test"]
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer = ByteLevelBPETokenizer()
 
 batch_size = 1000
 
@@ -17,10 +18,10 @@ def batch_iterator():
         yield dataset2[i : i + batch_size]["whole_func_string"]
 
 
-new_tokenizer = tokenizer.train_new_from_iterator(
+new_tokenizer = tokenizer.train_from_iterator(
     batch_iterator(),
     vocab_size=128000,
-    new_special_tokens=[
+    special_tokens=[
         "<|end_of_text|>",
         "<|begin_of_text|>",
         "<|im_end|>",
@@ -28,4 +29,5 @@ new_tokenizer = tokenizer.train_new_from_iterator(
     ],
 )
 
-new_tokenizer.push_to_hub("aeonium/Aeonium-v1-Base-7B")
+tokenizer = PreTrainedTokenizerFast(new_tokenizer)
+tokenizer.push_to_hub("aeonium/Aeonium-v1-Base-7B")
