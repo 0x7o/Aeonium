6b42c8e10c8840d6d45cd8e5398ceb6a31e869ae
0x7o <danil.kononyuk@ya.ru>
2024-04-23 17:57:23 +0800

    Changes

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -12,22 +12,22 @@ batch_size = 1000
 
 def batch_iterator():
     for i in range(0, len(dataset1), batch_size):
-        yield dataset1[i : i + batch_size]["text"]
+        yield dataset1[i: i + batch_size]["text"]
 
     for i in range(0, len(dataset2), batch_size):
-        yield dataset2[i : i + batch_size]["whole_func_string"]
+        yield dataset2[i: i + batch_size]["whole_func_string"]
 
 
 new_tokenizer = tokenizer.train_from_iterator(
     batch_iterator(),
-    vocab_size=128000,
+    vocab_size=64000,
     special_tokens=[
         "<|end_of_text|>",
-        "<|begin_of_text|>",
         "<|im_end|>",
         "<|im_start|>",
     ],
 )
 
-tokenizer = PreTrainedTokenizerFast(new_tokenizer)
+tokenizer = PreTrainedTokenizerFast(tokenizer_object=new_tokenizer, bos_token="<|end_of_text|>",
+                                    eos_token="<|end_of_text|>", )
 tokenizer.push_to_hub("aeonium/Aeonium-v1-Base-7B")
