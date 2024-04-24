22b0b470d96e637824e38b4a0b85c624b623cee8
0x7o <danil.kononyuk@ya.ru>
2024-04-24 18:28:13 +0800

    CulturaX tokenizer

--- data/culturax.py
+++ data/culturax.py
@@ -63,4 +63,4 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=1000)
     args = parser.parse_args()
 
-    main(args.output_dir, args.num_workers)
+    main(args.output_dir, args.batch_size)
