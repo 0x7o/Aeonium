285f2208e2a1a2ceb78f5ef216eb710acd1b8229
0x7o <danil.kononyuk@ya.ru>
2024-04-24 18:25:55 +0800

    CulturaX tokenizer

--- data/culturax.py
+++ data/culturax.py
@@ -1,3 +1,4 @@
+from datasets import Dataset
 from transformers import AutoTokenizer
 from multiprocessing import Pool
 import pyarrow.parquet as pq
@@ -21,29 +22,23 @@ def download_file(file_path: str, url: str):
 
     with open(file_path, "wb") as file:
         for chunk in tqdm(
-                response.iter_content(chunk_size=128),
-                total=int(response.headers.get("content-length", 0)) / 128,
+            response.iter_content(chunk_size=128),
+            total=int(response.headers.get("content-length", 0)) / 128,
         ):
             file.write(chunk)
 
 
-def parquet_iterator(file_path):
-    table = pq.read_table(file_path)
-    for record in table[0]:
-        yield str(record)
-
-
 def process_text(text: str):
-    return tokenizer.encode_plus(text, add_special_tokens=True, return_tensors="pt")
+    return tokenizer.encode_plus(text)
 
 
 def save_pickle(data, file_path):
-    with open(file_path, "wb") as file:
+    with open(file_path, "wb"):
         pickle.dump(data, file_path)
 
 
-def worker_task(text_batch):
-    return [process_text(text) for text in tqdm(text_batch)]
+def tokenization(example):
+    return tokenizer(example["text"])
 
 
 def main(output_dir: str, num_workers: int):
@@ -54,15 +49,10 @@ def main(output_dir: str, num_workers: int):
         url = f"https://huggingface.co/datasets/uonlp/CulturaX/resolve/main/ru/ru_part_{str(i).zfill(5)}.parquet?download=true"
         download_file(file_path, url)
 
-        data_iter = list(parquet_iterator(file_path))
-        chunk_size = len(data_iter) // num_workers
+        table = pq.read_table(file_path)
+        dataset = Dataset.from_pandas(table.to_pandas())
 
-        chunks = [data_iter[i * chunk_size:(i + 1) * chunk_size] for i in range(num_workers)]
-
-        with Pool(num_workers) as pool:
-            results = pool.map(worker_task, chunks)
-
-        results = [item for sublist in results for item in sublist]
+        results = dataset.map(tokenization, batched=True)
 
         save_pickle(results, f"{output_dir}/ru_part_{str(i).zfill(5)}.pkl")
         os.remove(file_path)
