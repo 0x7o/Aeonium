dbf7f2b6f2b6184f72a268847bcd3eed8b0ac5e4
0x7o <danil.kononyuk@ya.ru>
2024-04-24 18:21:34 +0800

    CulturaX tokenizer

--- data/culturax.py
+++ data/culturax.py
@@ -33,13 +33,17 @@ def parquet_iterator(file_path):
         yield str(record)
 
 
-def process_batch(batch):
-    return tokenizer.batch_encode_plus(batch)
+def process_text(text: str):
+    return tokenizer.encode_plus(text, add_special_tokens=True, return_tensors="pt")
 
 
 def save_pickle(data, file_path):
-    with open(file_path, "wb"):
+    with open(file_path, "wb") as file:
         pickle.dump(data, file_path)
+
+
+def worker_task(text_batch):
+    return [process_text(text) for text in text_batch]
 
 
 def main(output_dir: str, num_workers: int):
@@ -56,7 +60,7 @@ def main(output_dir: str, num_workers: int):
         chunks = [data_iter[i * chunk_size:(i + 1) * chunk_size] for i in range(num_workers)]
 
         with Pool(num_workers) as pool:
-            results = pool.map(process_batch, chunks)
+            results = pool.map(worker_task, chunks)
 
         results = [item for sublist in results for item in sublist]
 
