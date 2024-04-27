1cc09ea48fc964b9a9180aa675ed373c52ad5f29
0x7o <danil.kononyuk@ya.ru>
2024-04-27 15:24:25 +0800

    CulturaX upload to GCS

--- data/culturax.py
+++ data/culturax.py
@@ -1,14 +1,11 @@
-from datasets import Dataset
-from transformers import AutoTokenizer
 import pyarrow.parquet as pq
 from tqdm import tqdm
-import argparse
-import pyarrow as pa
 import requests
+import mlxu
+import json
 import os
 
 hf_token = os.getenv("HF_TOKEN")
-tokenizer = AutoTokenizer.from_pretrained("aeonium/Aeonium-v1-Base-7B")
 
 
 def download_file(file_path: str, url: str):
@@ -27,42 +24,23 @@ def download_file(file_path: str, url: str):
             file.write(chunk)
 
 
-def process_text(text: str):
-    return tokenizer.encode_plus(text)
-
-
-def save_pickle(data, file_path):
-    table = pa.Table.from_arrays([data], names=["tokens"])
-    pq.write_table(table, file_path)
-
-
-def tokenization(example):
-    return tokenizer(example["text"])
-
-
-def main(output_dir: str, batch_size: int):
-    os.makedirs(output_dir, exist_ok=True)
-
+def main():
     for i in tqdm(range(0, 1535), desc="Processing files..."):
-        file_path = f"{output_dir}/ru_part_{str(i).zfill(5)}.parquet"
+        file_path = f"ru_part_{str(i).zfill(5)}.parquet"
         url = f"https://huggingface.co/datasets/uonlp/CulturaX/resolve/main/ru/ru_part_{str(i).zfill(5)}.parquet?download=true"
         download_file(file_path, url)
 
         table = pq.read_table(file_path)
-        dataset = Dataset.from_pandas(table.to_pandas())
 
-        with open(f"{output_dir}/ru_part_{str(i).zfill(5)}.parquet.gzip", "wb") as f:
-            dataset = dataset.map(tokenization, batched=True, batch_size=batch_size)
-            n = sum([len(x) for x in dataset.to_dict()["input_ids"]])
-            print(f"\n\n\n\n\n{n}\n\n\n\n")
+        with mlxu.utils.open_file(
+            f"gs://aeonium-checkpoints/culturax/ru_part_{str(i).zfill(5)}.json", "w"
+        ) as file:
+            for item in table[0]:
+                text = str(item)
+                file.write(json.dumps({"text": text}) + "\n")
 
         os.remove(file_path)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--output_dir", type=str, required=True)
-    parser.add_argument("--batch_size", type=int, default=1000)
-    args = parser.parse_args()
-
-    main(args.output_dir, args.batch_size)
+    main()
